356c7aa57762fc0c51422f2402dbf410e114f984
xlnl <lamanhnle@gmail.com>
2020-10-24 17:55:04 -0400

    finished set up and created pokedex database and pokemon model, finished answered probing questions and mapping out what I need to do for the MVP

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const db = require("./models");
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
@@ -9,16 +10,32 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static("public")); 
 
 // GET / - main index of site
 app.get('/', function(req, res) {
-  const pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  const pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151/';
   // Use request to call the API
   axios.get(pokemonUrl).then( function(apiResponse) {
     const pokemon = apiResponse.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
 });
+
+// How does the app retrieve a list of Pokemon?
+//    from pokedex api! 
+// How many Pokemon does the API call retrieve? Why that many?
+//    20 because it's the default limit > so have to change the url to: "http://pokeapi.co/api/v2/pokemon?limit=151/" to display all 151
+// What are the routes defined in the application?
+//    "/pokemon" & "/""
+// Think about adding a Pokemon to your favorites.
+// How will this data be submitted?
+//    through a form with a method: post & action:"/pokemon"
+// What will you have to do to save this data to a database?
+//    we would use a post route to the /pokemon route and use a db method to findOrCreate the data to add it to our fave database
+// What will you have to do to display favorite Pokemon?
+//    we would use a get route with a db method find all favs and render the list of favorites
+
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,12 +5,20 @@ var router = express.Router();
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
   res.send('Render a page of favorites here');
+  // db.pokemon.findAll()
+  // render results file of that pokemon
 });
+
+
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
   res.send(req.body);
+  // db.pokemon.findOrCreate()
+  // redirect this to /pokemon
 });
+
+
 
 module.exports = router;
