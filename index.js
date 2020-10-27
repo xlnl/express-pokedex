06504082e171c4b79fb9fc545e04d5c301efe22d
xlnl <lamanhnle@gmail.com>
2020-10-27 16:19:49 -0400

    finished styling, centering is a struggle though

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use(methodOverride('_method'));
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
@@ -21,21 +22,6 @@ app.get('/', function(req, res) {
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
 });
-
-// How does the app retrieve a list of Pokemon?
-//    from pokedex api! 
-// How many Pokemon does the API call retrieve? Why that many?
-//    20 because it's the default limit > so have to change the url to: "http://pokeapi.co/api/v2/pokemon?limit=151/" to display all 151
-// What are the routes defined in the application?
-//    "/pokemon" & "/""
-// Think about adding a Pokemon to your favorites.
-// How will this data be submitted?
-//    through a form with a method: post & action:"/pokemon"
-// What will you have to do to save this data to a database?
-//    we would use a post route to the /pokemon route and use a db method to findOrCreate the data to add it to our fave database
-// What will you have to do to display favorite Pokemon?
-//    we would use a get route with a db method find all favs and render the list of favorites
-
 
 // Imports all routes from the pokemon routes file 
 app.use('/pokemon', require('./routes/pokemon'));
